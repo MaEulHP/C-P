3c05230459d0e309dfd3bc3f4a34c0c7b80c9c0b
MaEulHP <66859688+MaEulHP@users.noreply.github.com>
2023-09-11 15:30:41 +0900

    이동 테스트
    1

--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerClass : MonoBehaviour
 {
+
+    public Rigidbody player_rigidbody;
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,7 @@ public class PlayerClass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow))
+            player_rigidbody.AddForce(-speed, 0f, 0f);    
     }
 }
