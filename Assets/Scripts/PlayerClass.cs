c20fff1d481d0addb99eadd94096c752c3afaa8b
MaEulHP <66859688+MaEulHP@users.noreply.github.com>
2023-09-13 09:35:41 +0900

    이동 스크립트
    키보드 방향키, 스페이스바

--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -17,7 +17,28 @@ public class PlayerClass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
-            player_rigidbody.AddForce(-speed, 0f, 0f);    
+        // move by keyboard
+        float translation = Input.GetAxis("Vertical") * speed;
+        translation *= Time.deltaTime;
+
+        if ((Input.GetKey(KeyCode.Space))||(Input.GetKey(KeyCode.W)))
+        {
+            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0,0,-1) * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime*speed);
+        }
+
+        if( Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * speed);
+        }    
     }
 }
