66a33f4a0d6a63b892117333a16c2d0251d5ee5c
MaEulHP <boy123753@gmail.com>
2023-09-19 12:50:59 +0900

    필요한 변수들 추가

--- Assets/Scripts/PlayerClass.cs
+++ Assets/Scripts/PlayerClass.cs
@@ -2,11 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//enum State = {"Ready", "Attack", "Guard", "Death"};
+
 public class PlayerClass : MonoBehaviour
 {
 
-    public Rigidbody player_rigidbody;
+    public Rigidbody2D player_rigidbody;
     public float speed = 5f;
+    public GameObject bottom ;
+    //public State player_State = 0;
+
+    void Awake()
+    {
+        player_rigidbody = GetComponent<Rigidbody2D>();
+        player_rigidbody.freezeRotation = true;
+    }
+
+    public int counter_Token = 2;
+    public float counter_Gauge = 0.0f;
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -17,19 +32,14 @@ public class PlayerClass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // move by keyboard
+        //Move by Key Input
         float translation = Input.GetAxis("Vertical") * speed;
         translation *= Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.Space))//||(Input.GetKey(KeyCode.W)))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            transform.Translate(new Vector2(0, 1) * Time.deltaTime * speed);
+            player_rigidbody.AddForce(Vector2.up * speed, ForceMode2D.Impulse);
         }
-
-        /*if (Input.GetKey(KeyCode.DownArrow)||Input.GetKey(KeyCode.S))
-        {
-            transform.Translate(new Vector2(0,-1) * Time.deltaTime * speed);
-        }*/
 
         if(Input.GetKey(KeyCode.LeftArrow)||Input.GetKey(KeyCode.A))
         {
@@ -39,6 +49,17 @@ public class PlayerClass : MonoBehaviour
         if( Input.GetKey(KeyCode.RightArrow)||Input.GetKey(KeyCode.D))
         {
             transform.Translate(new Vector2(1, 0) * Time.deltaTime * speed);
-        }    
+        }
+
+        //Counter Token and Gauge Management
+        if(counter_Gauge <= 100) {
+            counter_Gauge += 0.01f;
+        }
+        else {
+            if(counter_Token < 2){
+                counter_Token++;
+                counter_Gauge -= 100;
+            }
+        }   
     }
 }
